bda30d8e356eca1204c1498eaafaa13af8d75fad
mdiedricks <michael.diedricks@gmail.com>
2019-09-07 16:11:40 +1000

    js file updated

---
+++ project00.js
@@ -0,0 +1,31 @@
+//new game function should start a new game object
+function NewGame(player1, player2){
+    this.player1 = player1;
+    this.player2 = player2;
+}
+
+//add a new player to the list
+function newPlayer(name, ){
+    pass;
+}
+
+//choose player names for X and O
+function selPlayers(playerX, playerO){
+    console.log(player1);
+}
+
+//place a symbol in a grid cell
+function makeMove(curPlayer, input){
+    console.log(curPlayer);
+}
+
+//change active player after a symbol has been placed
+function swapPlayer(curPlayer){
+    console.log(curPlayer);
+}
+
+//check if somebody has won, after each move has been made
+
+
+//end game once game has been won
+
