27e54aa996128e24602ebb4c47c21f1ab418259a
mdiedricks <michael.diedricks@gmail.com>
2019-09-10 19:51:01 +1000

    fixed

--- script.js
+++ script.js
@@ -16,47 +16,47 @@ const Game = function (player1, player2){
     this.secPlayer = player2;
     this.curPlayer = fstPlayer;
     
-    // //create array values for the game board
-    // this.gameBoard = [" "," "," "," "," "," "," "," "," "],
-    // //make move on the game board
-    // this.makeMove = function(input){
-    //     //store active player in variable
-    //     if (curPlayer === fstPlayer){
-    //         let symbol ='X';
-    //     }else {
-    //         let symbol = 'O';
-    //     }
-    //     //select a location to place a symbol
-    //     this.gameBoard[input] = symbol;
-    //     return;
-    // };
-//     //function to check if a win combination is achieved
-//     this.checkWin = function(arr){
-//         let tempArr = arr;
-//         if (curPlayer === fstPlayer){
-//             for (i in tempArr){
-//                 if (i === 'X'){
-//                     arr[i] = 1;
-//                 } else {
-//                     arr[i] = 0;
-//                 }
-//             }
-//         } else {
-//             for (i in tempArr){
-//                 if (i === 'O'){
-//                     arr[i] = 1;
-//                 } else {
-//                     arr[i] = 0;
-//                 }
-//             }
-//         }
-//     }
-// }
+    //create array values for the game board
+    this.gameBoard = [" "," "," "," "," "," "," "," "," "],
+    //make move on the game board
+    this.makeMove = function(input){
+        //store active player in variable
+        if (curPlayer === fstPlayer){
+            let symbol ='X';
+        }else {
+            let symbol = 'O';
+        }
+        //select a location to place a symbol
+        this.gameBoard[input] = symbol;
+        return;
+    };
+    //function to check if a win combination is achieved
+    this.checkWin = function(arr){
+        let tempArr = arr;
+        if (curPlayer === fstPlayer){
+            for (i in tempArr){
+                if (i === 'X'){
+                    arr[i] = 1;
+                } else {
+                    arr[i] = 0;
+                }
+            }
+        } else {
+            for (i in tempArr){
+                if (i === 'O'){
+                    arr[i] = 1;
+                } else {
+                    arr[i] = 0;
+                }
+            }
+        }
+    }
+}
 
-// //add a new player to the list
-// function newPlayer(name){
-//     players.push(name);
-// }
+//add a new player to the list
+function newPlayer(name){
+    players.push(name);
+}
 
 
 //end game once game has been won
